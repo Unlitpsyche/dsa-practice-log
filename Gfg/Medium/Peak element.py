33005d6840b189cef48b2c766d7127241f7c30a2
Ankita <144545049+Unlitpsyche@users.noreply.github.com>
2026-07-31 09:22:47 +0530

    Update Peak element.py

--- Gfg/Medium/Peak element.py
+++ Gfg/Medium/Peak element.py
@@ -27,5 +27,5 @@ class Solution:
             if arr[mid] < arr[mid+1]:
                 low = mid + 1
             else:
-                high = mid - 1
-        return mid
+                high = mid
+        return low
